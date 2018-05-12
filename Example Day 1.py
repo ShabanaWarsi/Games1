3537777389789ec34c0c1bcc85d9b3e18d1e914b
ShabanaWarsi <31715633+ShabanaWarsi@users.noreply.github.com>
2018-05-12 12:46:32 -0400

    Add files via upload

---
+++ Example Day 1.py
@@ -0,0 +1,8 @@
+# our first program
+
+employee = input("enter employee's name: ")
+hours = int(input ("enter hours worked: "))
+pay_rate = float(input ("enter pay rate: " ))
+gross_pay = hours * pay_rate
+print ()
+print (employee, gross_pay)
